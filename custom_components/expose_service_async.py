5d7bb1c3472e81152b7299361cc70c6a6c96669c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-07-06 21:38:37 -0700

    Create expose_service_async.py

---
+++ custom_components/expose_service_async.py
@@ -0,0 +1,24 @@
+"""Example of a custom component exposing a service."""
+import asyncio
+import logging
+
+from homeassistant.core import callback
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "expose_service_sync"
+_LOGGER = logging.getLogger(__name__)
+
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Setup the service example component."""
+    @callback
+    def my_service(call):
+        """My first service."""
+        _LOGGER.info('Received data', call.data)
+
+    # Register our service with Home Assistant.
+    hass.services.async_register(DOMAIN, 'demo', my_service)
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
