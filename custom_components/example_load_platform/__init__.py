3b695a6730372bee7b805ee2785d6f4b8b2eeef9
Martin Hjelmare <marhje52@kth.se>
2019-07-25 20:19:58 +0200

    Clean up sensor examples (#12)
    * Fix lint issues.
    * Follow current standard.

--- custom_components/example_load_platform/__init__.py
+++ custom_components/example_load_platform/__init__.py
@@ -1,11 +1,12 @@
 """Example Load Platform integration."""
 DOMAIN = 'example_load_platform'
 
+
 def setup(hass, config):
     """Your controller/hub specific code."""
     # Data that you want to share with your platforms
     hass.data[DOMAIN] = {
-      'temperature': 23
+        'temperature': 23
     }
 
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
