89655150113da196e59a8dc034f6f01aefef2234
G Johansson <goran.johansson@shiftit.se>
2022-01-07 23:49:30 +0100

    Change example to newest development

--- custom_components/example_sensor/sensor.py
+++ custom_components/example_sensor/sensor.py
@@ -24,25 +24,12 @@ class ExampleSensor(SensorEntity):
     def __init__(self):
         """Initialize the sensor."""
         self._state = None
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return 'Example Temperature'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self) -> str:
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        self._attr_name = 'Example Temperature'
+        self._attr_native_unit_of_measurement = TEMP_CELSIUS
 
     def update(self) -> None:
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = 23
+        self._attr_native_value = 23
