b4560d9742564ef7f14a6234b1f4495851091464
Christoph <Christoph@Caina.de>
2024-07-14 01:31:17 +0200

    Update __init__.py

--- custom_components/detailed_hello_world_push/__init__.py
+++ custom_components/detailed_hello_world_push/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from hoomeassistant.const import Platform
+from homeassistant.const import Platform
 
 from . import hub
 from .const import DOMAIN
